613d8c34ee565f3cadce99af64ff6eda20221675
Crowdbotics <team@crowdbotics.com>
2024-01-18 14:50:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cuisinecraft-45431",
+  url: "https://cuisinecraft-45431.botics.co",
+  api: "https://cuisinecraft-45431.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
